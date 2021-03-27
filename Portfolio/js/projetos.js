78ed4302eadae5742c01974eeb14960f7dc61b5c
nicolasian <nicolasoliveeira@gmail.com>
2021-03-26 23:19:40 -0300

    Mudancas responsivas
    Resolvido bug em que aparecia uma parte branca nos cards ao redimensionar

---
+++ Portfolio/js/projetos.js
@@ -0,0 +1,11 @@
+$(".desk-logo").hover(function() {
+    $(this).animate({
+        left: "10px"
+    }, 100);
+    $(this).text("nicolas.com");
+  }, function() {
+    $(this).animate({
+        left: "0px"
+    }, 100)
+    $(this).text("nicolas.");
+  });
